cdd5cc708abb5f931de61d4d9abdcafaf808d869
wet333 <wet.4gustin@gmail.com>
2023-11-15 14:36:49 -0300

    Skills ordered using api new feature

--- website/js/resume/skills.js
+++ website/js/resume/skills.js
@@ -21,11 +21,24 @@ function renderResumeSkillList(data) {
     const skills = data;
     const skillsList = document.getElementById("skills-list");
 
+    // order skills list using skill.order property (if exists) from 0 to n
+    skills.sort((a, b) => {
+        if (a.order && b.order) {
+            return a.order - b.order;
+        }
+        return 0;
+    });
+
     const skillsListItems = skills.map((skill) => {
         const { name, icon } = skill;
         const iconSize = 64;
         const iconElement = icon ? "<img src='" + icon + "' alt='" + name + "' width='" + iconSize + "' height='" + iconSize + "' />" : "";
-        return `<li><div class='skill-item' > ${iconElement}${name} </div></li>`;
+
+        if (skill.order && skill.order >= 0) {
+            return `<li style='order: ${skill.order}'><div class='skill-item' > ${iconElement}${name} </div></li>`;
+        } else {
+            return `<li style='order: 9999'><div class='skill-item' > ${iconElement}${name} </div></li>`;
+        }
     });
     
     skillsList.innerHTML = skillsListItems.join("");
